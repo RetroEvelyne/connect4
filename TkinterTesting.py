f26f22ce83e2efdabb78ce377643dde21c19bc9a
Evelyne <retroevelyne@outlook.com>
2022-09-24 20:06:12 +0100

    initial commit

---
+++ TkinterTesting.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+def CreateGUI():
+    connect4 = Tk()
+    connect4.title("Connect Four!")
+    connect4.geometry("500x220")
+
+
+if __name__ == "__main__":
+    CreateGUI()
